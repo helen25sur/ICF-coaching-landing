45688fffb17ed60825269958af2a674c94c11454
Olena Surilova <elen21surilova@gmail.com>
2024-04-06 20:34:15 +0300

    fix: add the second part to HeroSection component

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const HeroSection = () => {
   return (
-    <section className="relative pb-[80px] pt-[80px] dark:bg-dark lg:pt-[120px]">
+    <section id="about_us" className="relative pb-[80px] pt-[80px] dark:bg-dark lg:pt-[120px]">
       <div className="container mx-auto">
         <div className="-mx-4 flex flex-wrap items-center">
           <div className="w-full px-4 lg:w-1/2">
             <div className="mb-12 lg:mb-0">
-              <h2 className="mb-5 text-3xl font-bold leading-[1.208] text-dark dark:text-white lg:text-[24px] xl:text-[28px]">
+              <h2 className="mb-5 text-3xl font-bold leading-[1.208] text-primary dark:text-blue-200 lg:text-[24px] xl:text-[28px]">
                 ПАРТНЕРСЬКИЙ ПРОЄКТ
               </h2>
               <h1 className="mb-5 text-3xl font-bold leading-[1.208] text-dark dark:text-white lg:text-[28px] xl:text-[32px]">«Кар’єрне консультування з коучинговою підтримкою»</h1>
@@ -17,10 +17,10 @@ const HeroSection = () => {
                 коучів ICF  <br />
                 волонтерського проєкту WinE
               </h3>
-              <h4 className="mb-5 text-2xl text-dark dark:text-white"><data>16-30 квітня 2024 року</data></h4>
-              <nav className="dark:text-dark-6 mb-8 max-w-[485px] text-base text-body-color">
+              <h4 className="mb-5 text-2xl text-dark dark:text-white"><data className="text-primary dark:text-blue-200">16-30 квітня 2024 року</data></h4>
+              <div className="dark:text-dark-6 mb-8 max-w-[485px] text-base text-body-color">
                 <ul className="space-y-3">
-                  <li className="flex text-base text-body-color dark:text-dark-6">
+                  <li className="flex text-base text-dark-3 dark:text-dark-8">
                     <span className="mr-2.5 mt-0.5 text-primary">
                       <svg
                         width="20"
@@ -35,8 +35,8 @@ const HeroSection = () => {
                         />
                       </svg>
                     </span>
-                    закінчили курси і не знаєте, з чого почати пошук роботи? закінчили курси і не знаєте, з чого почати пошук роботи? </li>
-                  <li className="flex text-base text-body-color dark:text-dark-6">
+                    закінчили курси і не знаєте, з чого почати пошук роботи?</li>
+                  <li className="flex text-base text-dark-3 dark:text-dark-8">
                     <span className="mr-2.5 mt-0.5 text-primary">
                       <svg
                         width="20"
@@ -52,7 +52,7 @@ const HeroSection = () => {
                       </svg>
                     </span>
                     важко знайти вакансії для початківців?</li>
-                  <li className="flex text-base text-body-color dark:text-dark-6">
+                  <li className="flex text-base text-dark-3 dark:text-dark-8">
                     <span className="mr-2.5 mt-0.5 text-primary">
                       <svg
                         width="20"
@@ -68,7 +68,7 @@ const HeroSection = () => {
                       </svg>
                     </span>
                     немає відгуків на резюме?</li>
-                  <li className="flex text-base text-body-color dark:text-dark-6">
+                  <li className="flex text-base text-dark-3 dark:text-dark-8">
                     <span className="mr-2.5 mt-0.5 text-primary">
                       <svg
                         width="20"
@@ -84,7 +84,7 @@ const HeroSection = () => {
                       </svg>
                     </span>
                     хвилюєтеся перед співбесідою?</li>
-                  <li className="flex text-base text-body-color dark:text-dark-6">
+                  <li className="flex text-base text-dark-3 dark:text-dark-8">
                     <span className="mr-2.5 mt-0.5 text-primary">
                       <svg
                         width="20"
@@ -100,7 +100,7 @@ const HeroSection = () => {
                       </svg>
                     </span>
                     не отримуєте фідбеку від рекрутерів?</li>
-                  <li className="flex text-base text-body-color dark:text-dark-6">
+                  <li className="flex text-base text-dark-3 dark:text-dark-8">
                     <span className="mr-2.5 mt-0.5 text-primary">
                       <svg
                         width="20"
@@ -117,7 +117,7 @@ const HeroSection = () => {
                     </span>
                     хочете світчнутися в ІТ, але не впевнені, що знайдете роботу?</li>
                 </ul>
-              </nav>
+              </div>
             </div>
           </div>
 
@@ -140,6 +140,122 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
+      <section class="dark:bg-dark-2 bg-tg-bg py-20">
+        <div className="container mx-auto">
+          <div className="-mx-4 flex flex-wrap items-center">
+            <div className="w-full px-4 lg:w-1/2 xl:w-5/12">
+              <div>
+                <h2 className="text-2xl font-bold text-dark dark:text-white sm:text-4xl lg:text-2xl xl:text-[28px] 2xl:text-3xl">
+                  ЗНАЙОМО?<br />
+                  Запрошуємо на наш безкоштовний вебінар <date className="text-primary dark:text-blue-200">16.04.2024</date>, після якого ти:
+                </h2>
+              </div>
+            </div>
+            <div className="w-full px-4 lg:w-1/2 xl:w-7/12">
+              <div className="mt-12 flex flex-wrap items-center justify-center lg:mt-0 xl:justify-end">
+                <ul className="space-y-3">
+                  <li className="flex text-xl text-dark-3 dark:text-dark-8">
+                    <span className="mr-2.5 text-primary">
+                      <svg
+                        width="20"
+                        height="20"
+                        viewBox="0 0 20 20"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <g clip-path="url(#clip0_980_24878)">
+                          <path
+                            d="M5.03124 19.4375C4.74999 19.4375 4.46874 19.3437 4.24999 19.1875C3.81249 18.875 3.56249 18.3125 3.65624 17.7812L4.46874 12.75L0.968744 9.15625C0.593744 8.78125 0.468744 8.21875 0.624994 7.6875C0.781244 7.1875 1.21874 6.8125 1.71874 6.75L6.56249 5.96875L8.74999 1.375C8.99999 0.875 9.46874 0.5625 9.99999 0.5625C10.5312 0.5625 11.0312 0.875 11.25 1.375L13.4375 5.9375L18.25 6.6875C18.75 6.78125 19.1875 7.125 19.3437 7.625C19.5312 8.15625 19.375 8.71875 19 9.09375L15.5312 12.7187L16.3437 17.7812C16.4375 18.3437 16.2187 18.875 15.75 19.1875C15.3125 19.5 14.7812 19.5312 14.3125 19.2812L9.99999 16.9375L5.68749 19.2812C5.49999 19.4062 5.24999 19.4375 5.03124 19.4375ZM1.96874 8.125C1.96874 8.125 1.96874 8.15625 1.96874 8.1875L5.62499 11.9375C5.84374 12.1562 5.93749 12.5 5.90624 12.8125L5.06249 18.0312C5.06249 18.0312 5.06249 18.0312 5.06249 18.0625L9.56249 15.625C9.84374 15.4687 10.1875 15.4687 10.5 15.625L15 18.0625C15 18.0625 15 18.0625 15 18.0312L14.1562 12.7812C14.0937 12.4688 14.2187 12.1562 14.4375 11.9062L18.0937 8.15625C18.125 8.125 18.0937 8.09375 18.0937 8.09375L13.0625 7.3125C12.75 7.25 12.4687 7.0625 12.3437 6.75L9.99999 2L7.74999 6.78125C7.62499 7.0625 7.34374 7.28125 7.03124 7.34375L1.96874 8.125Z"
+                            fill="currentColor"
+                          />
+                        </g>
+                        <defs>
+                          <clipPath id="clip0_980_24878">
+                            <rect width="20" height="20" fill="white" />
+                          </clipPath>
+                        </defs>
+                      </svg>
+                    </span>
+                    Створиш свою стратегію кар’єри в ІТ
+                  </li>
+                  <li className="flex text-xl text-dark-3 dark:text-dark-8">
+                    <span className="mr-2.5 text-primary">
+                      <svg
+                        width="20"
+                        height="20"
+                        viewBox="0 0 20 20"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <g clip-path="url(#clip0_980_24878)">
+                          <path
+                            d="M5.03124 19.4375C4.74999 19.4375 4.46874 19.3437 4.24999 19.1875C3.81249 18.875 3.56249 18.3125 3.65624 17.7812L4.46874 12.75L0.968744 9.15625C0.593744 8.78125 0.468744 8.21875 0.624994 7.6875C0.781244 7.1875 1.21874 6.8125 1.71874 6.75L6.56249 5.96875L8.74999 1.375C8.99999 0.875 9.46874 0.5625 9.99999 0.5625C10.5312 0.5625 11.0312 0.875 11.25 1.375L13.4375 5.9375L18.25 6.6875C18.75 6.78125 19.1875 7.125 19.3437 7.625C19.5312 8.15625 19.375 8.71875 19 9.09375L15.5312 12.7187L16.3437 17.7812C16.4375 18.3437 16.2187 18.875 15.75 19.1875C15.3125 19.5 14.7812 19.5312 14.3125 19.2812L9.99999 16.9375L5.68749 19.2812C5.49999 19.4062 5.24999 19.4375 5.03124 19.4375ZM1.96874 8.125C1.96874 8.125 1.96874 8.15625 1.96874 8.1875L5.62499 11.9375C5.84374 12.1562 5.93749 12.5 5.90624 12.8125L5.06249 18.0312C5.06249 18.0312 5.06249 18.0312 5.06249 18.0625L9.56249 15.625C9.84374 15.4687 10.1875 15.4687 10.5 15.625L15 18.0625C15 18.0625 15 18.0625 15 18.0312L14.1562 12.7812C14.0937 12.4688 14.2187 12.1562 14.4375 11.9062L18.0937 8.15625C18.125 8.125 18.0937 8.09375 18.0937 8.09375L13.0625 7.3125C12.75 7.25 12.4687 7.0625 12.3437 6.75L9.99999 2L7.74999 6.78125C7.62499 7.0625 7.34374 7.28125 7.03124 7.34375L1.96874 8.125Z"
+                            fill="currentColor"
+                          />
+                        </g>
+                        <defs>
+                          <clipPath id="clip0_980_24878">
+                            <rect width="20" height="20" fill="white" />
+                          </clipPath>
+                        </defs>
+                      </svg>
+                    </span>
+                    Розвинеш навички самопрезентації
+                  </li>
+                  <li className="flex text-xl text-dark-3 dark:text-dark-8">
+                    <span className="mr-2.5 text-primary">
+                      <svg
+                        width="20"
+                        height="20"
+                        viewBox="0 0 20 20"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <g clip-path="url(#clip0_980_24878)">
+                          <path
+                            d="M5.03124 19.4375C4.74999 19.4375 4.46874 19.3437 4.24999 19.1875C3.81249 18.875 3.56249 18.3125 3.65624 17.7812L4.46874 12.75L0.968744 9.15625C0.593744 8.78125 0.468744 8.21875 0.624994 7.6875C0.781244 7.1875 1.21874 6.8125 1.71874 6.75L6.56249 5.96875L8.74999 1.375C8.99999 0.875 9.46874 0.5625 9.99999 0.5625C10.5312 0.5625 11.0312 0.875 11.25 1.375L13.4375 5.9375L18.25 6.6875C18.75 6.78125 19.1875 7.125 19.3437 7.625C19.5312 8.15625 19.375 8.71875 19 9.09375L15.5312 12.7187L16.3437 17.7812C16.4375 18.3437 16.2187 18.875 15.75 19.1875C15.3125 19.5 14.7812 19.5312 14.3125 19.2812L9.99999 16.9375L5.68749 19.2812C5.49999 19.4062 5.24999 19.4375 5.03124 19.4375ZM1.96874 8.125C1.96874 8.125 1.96874 8.15625 1.96874 8.1875L5.62499 11.9375C5.84374 12.1562 5.93749 12.5 5.90624 12.8125L5.06249 18.0312C5.06249 18.0312 5.06249 18.0312 5.06249 18.0625L9.56249 15.625C9.84374 15.4687 10.1875 15.4687 10.5 15.625L15 18.0625C15 18.0625 15 18.0625 15 18.0312L14.1562 12.7812C14.0937 12.4688 14.2187 12.1562 14.4375 11.9062L18.0937 8.15625C18.125 8.125 18.0937 8.09375 18.0937 8.09375L13.0625 7.3125C12.75 7.25 12.4687 7.0625 12.3437 6.75L9.99999 2L7.74999 6.78125C7.62499 7.0625 7.34374 7.28125 7.03124 7.34375L1.96874 8.125Z"
+                            fill="currentColor"
+                          />
+                        </g>
+                        <defs>
+                          <clipPath id="clip0_980_24878">
+                            <rect width="20" height="20" fill="white" />
+                          </clipPath>
+                        </defs>
+                      </svg>
+                    </span>
+                    Здобудеш впевненість у власних силах
+                  </li>
+                  <li className="flex text-xl text-dark-3 dark:text-dark-8">
+                    <span className="mr-2.5 text-primary">
+                      <svg
+                        width="20"
+                        height="20"
+                        viewBox="0 0 20 20"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <g clip-path="url(#clip0_980_24878)">
+                          <path
+                            d="M5.03124 19.4375C4.74999 19.4375 4.46874 19.3437 4.24999 19.1875C3.81249 18.875 3.56249 18.3125 3.65624 17.7812L4.46874 12.75L0.968744 9.15625C0.593744 8.78125 0.468744 8.21875 0.624994 7.6875C0.781244 7.1875 1.21874 6.8125 1.71874 6.75L6.56249 5.96875L8.74999 1.375C8.99999 0.875 9.46874 0.5625 9.99999 0.5625C10.5312 0.5625 11.0312 0.875 11.25 1.375L13.4375 5.9375L18.25 6.6875C18.75 6.78125 19.1875 7.125 19.3437 7.625C19.5312 8.15625 19.375 8.71875 19 9.09375L15.5312 12.7187L16.3437 17.7812C16.4375 18.3437 16.2187 18.875 15.75 19.1875C15.3125 19.5 14.7812 19.5312 14.3125 19.2812L9.99999 16.9375L5.68749 19.2812C5.49999 19.4062 5.24999 19.4375 5.03124 19.4375ZM1.96874 8.125C1.96874 8.125 1.96874 8.15625 1.96874 8.1875L5.62499 11.9375C5.84374 12.1562 5.93749 12.5 5.90624 12.8125L5.06249 18.0312C5.06249 18.0312 5.06249 18.0312 5.06249 18.0625L9.56249 15.625C9.84374 15.4687 10.1875 15.4687 10.5 15.625L15 18.0625C15 18.0625 15 18.0625 15 18.0312L14.1562 12.7812C14.0937 12.4688 14.2187 12.1562 14.4375 11.9062L18.0937 8.15625C18.125 8.125 18.0937 8.09375 18.0937 8.09375L13.0625 7.3125C12.75 7.25 12.4687 7.0625 12.3437 6.75L9.99999 2L7.74999 6.78125C7.62499 7.0625 7.34374 7.28125 7.03124 7.34375L1.96874 8.125Z"
+                            fill="currentColor"
+                          />
+                        </g>
+                        <defs>
+                          <clipPath id="clip0_980_24878">
+                            <rect width="20" height="20" fill="white" />
+                          </clipPath>
+                        </defs>
+                      </svg>
+                    </span>
+                    Наблизишся до свого вигідного оферу
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </section>
   )
 }
