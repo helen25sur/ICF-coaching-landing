7cb32986b8ccf0b4eda057e48912c306494438e0
Olena Surilova <elen21surilova@gmail.com>
2024-04-12 21:19:05 +0300

    fix: small changes

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -50,7 +50,7 @@ const Header = () => {
                   </a>
                 </li>
               </ul>
-              <div className={`justify-end pr-16 sm:flex lg:pr-0 ${mobileMenuOpen ? 'flex flex-col pr-8 mt-5' : 'hidden'}`}>
+              <div className={`justify-end pr-16 sm:flex lg:pr-0 ${mobileMenuOpen ? 'flex flex-col pr-2 mt-5' : 'hidden'}`}>
                 <a rel="noreferrer" target='_blank' href="https://docs.google.com/forms/d/e/1FAIpQLScBDB0gGnMrGYlOcxxGmAiN1JQ32L3bLohmOFLJAK7EzCjCLA/viewform" className="inline-flex items-center justify-center rounded-md border border-primary bg-primary px-7 py-3 text-center uppercase text-base font-medium text-white hover:border-[#1B44C8] hover:bg-[#1B44C8] active:border-[#1B44C8] active:bg-[#1B44C8] disabled:border-gray-3 disabled:bg-gray-3 disabled:text-dark-5">
                   Взяти участь
                 </a>
